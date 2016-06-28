b063d199b3dda37b2c336492a5931f153a358eef
Deiner Villalobos <dgvillalobosp@est.utn.ac.cr>
2016-06-27 22:45:26 -0600

    Mi primer comit

---
+++ guardar.js
@@ -0,0 +1,264 @@
+var num1=localStorage.getItem('actual1') || 0;
+var num2=localStorage.getItem('actual2') || 0;
+var num3=localStorage.getItem('actual3') || 0;
+var num4=localStorage.getItem('actual4') || 0;
+var num5=localStorage.getItem('actual5') || 0;
+var num6=localStorage.getItem('actual6') || 0;
+var num7=localStorage.getItem('actual7') || 0;
+var num8=localStorage.getItem('actual8') || 0;
+var num9=localStorage.getItem('actual9') || 0;
+var num10=localStorage.getItem('actual10') || 0;
+var num11=localStorage.getItem('actual11') || 0;
+var num12=localStorage.getItem('actual12') || 0;
+var num13=localStorage.getItem('actual13') || 0;
+var num14=localStorage.getItem('actual14') || 0;
+var num15=localStorage.getItem('actual15') || 0;
+var num16=localStorage.getItem('actual16') || 0;
+var num17=localStorage.getItem('actual17') || 0;
+var num18=localStorage.getItem('actual18') || 0;
+var num19=localStorage.getItem('actual19') || 0;
+var num20=localStorage.getItem('actual20') || 0;
+var num21=localStorage.getItem('actual21') || 0;
+var num22=localStorage.getItem('actual22') || 0;
+var num23=localStorage.getItem('actual23') || 0;
+var num24=localStorage.getItem('actual24') || 0;
+var num25=localStorage.getItem('actual25') || 0;
+var num26=localStorage.getItem('actual26') || 0;
+var num27=localStorage.getItem('actual27') || 0;
+var num28=localStorage.getItem('actual28') || 0;
+var num29=localStorage.getItem('actual29') || 0;
+var num30=localStorage.getItem('actual30') || 0;
+var num31=localStorage.getItem('actual31') || 0;
+var num32=localStorage.getItem('actual32') || 0;
+var num33=localStorage.getItem('actual33') || 0;
+var num34=localStorage.getItem('actual34') || 0;
+var num35=localStorage.getItem('actual35') || 0;
+var num36=localStorage.getItem('actual36') || 0;
+var num37=localStorage.getItem('actual37') || 0;
+var num38=localStorage.getItem('actual38') || 0;
+var num39=localStorage.getItem('actual39') || 0;
+var num40=localStorage.getItem('actual40') || 0;
+var num41=localStorage.getItem('actual41') || 0;
+var num42=localStorage.getItem('actual42') || 0;
+var num43=localStorage.getItem('actual43') || 0;
+var num44=localStorage.getItem('actual44') || 0;
+var num45=localStorage.getItem('actual45') || 0;
+var num46=localStorage.getItem('actual46') || 0;
+var num47=localStorage.getItem('actual47') || 0;
+var num48=localStorage.getItem('actual48') || 0;
+var num49=localStorage.getItem('actual49') || 0;
+var num50=localStorage.getItem('actual50') || 0;
+var num51=localStorage.getItem('actual51') || 0;
+var num52=localStorage.getItem('actual52') || 0;
+var num53=localStorage.getItem('actual53') || 0;
+var num54=localStorage.getItem('actual54') || 0;
+var num55=localStorage.getItem('actual55') || 0;
+var num56=localStorage.getItem('actual56') || 0;
+var num57=localStorage.getItem('actual57') || 0;
+var num58=localStorage.getItem('actual58') || 0;
+var num59=localStorage.getItem('actual59') || 0;
+var num60=localStorage.getItem('actual60') || 0;
+var num61=localStorage.getItem('actual61') || 0;
+var num62=localStorage.getItem('actual62') || 0;
+var num63=localStorage.getItem('actual63') || 0;
+var num64=localStorage.getItem('actual64') || 0;
+function clicks() {
+	num1 = parseInt(document.getElementById('num1').value)
+	num2 = parseInt(document.getElementById('num2').value)
+	num3 = parseInt(document.getElementById('num3').value)
+	num4 = parseInt(document.getElementById('num4').value)
+	num5 = parseInt(document.getElementById('num5').value)
+	num6 = parseInt(document.getElementById('num6').value)
+	num7 = parseInt(document.getElementById('num7').value)
+	num8 = parseInt(document.getElementById('num8').value)
+	num9 = parseInt(document.getElementById('num9').value)
+	num10 = parseInt(document.getElementById('num10').value)
+	num11 = parseInt(document.getElementById('num11').value)
+	num12 = parseInt(document.getElementById('num12').value)
+	num13 = parseInt(document.getElementById('num13').value)
+	num14 = parseInt(document.getElementById('num14').value)
+	num15 = parseInt(document.getElementById('num15').value)
+	num16 = parseInt(document.getElementById('num16').value)
+	num17 = parseInt(document.getElementById('num17').value)
+	num18 = parseInt(document.getElementById('num18').value)
+	num19 = parseInt(document.getElementById('num19').value)
+	num20 = parseInt(document.getElementById('num20').value)
+	num21 = parseInt(document.getElementById('num21').value)
+	num22 = parseInt(document.getElementById('num22').value)
+	num23 = parseInt(document.getElementById('num23').value)
+	num24 = parseInt(document.getElementById('num24').value)
+	num25 = parseInt(document.getElementById('num25').value)
+	num26 = parseInt(document.getElementById('num26').value)
+	num27 = parseInt(document.getElementById('num27').value)
+	num28 = parseInt(document.getElementById('num28').value)
+	num29 = parseInt(document.getElementById('num29').value)
+	num30 = parseInt(document.getElementById('num30').value)
+	num31 = parseInt(document.getElementById('num31').value)
+	num32 = parseInt(document.getElementById('num32').value)
+	num33 = parseInt(document.getElementById('num33').value)
+	num34 = parseInt(document.getElementById('num34').value)
+	num35 = parseInt(document.getElementById('num35').value)
+	num36 = parseInt(document.getElementById('num36').value)
+	num37 = parseInt(document.getElementById('num37').value)
+	num38 = parseInt(document.getElementById('num38').value)
+	num39 = parseInt(document.getElementById('num39').value)
+	num40 = parseInt(document.getElementById('num40').value)
+	num41 = parseInt(document.getElementById('num41').value)
+	num42 = parseInt(document.getElementById('num42').value)
+	num43 = parseInt(document.getElementById('num43').value)
+	num44 = parseInt(document.getElementById('num44').value)
+	num45 = parseInt(document.getElementById('num45').value)
+	num46 = parseInt(document.getElementById('num46').value)
+	num47 = parseInt(document.getElementById('num47').value)
+	num48 = parseInt(document.getElementById('num48').value)
+	num49 = parseInt(document.getElementById('num49').value)
+	num50 = parseInt(document.getElementById('num50').value)
+	num51 = parseInt(document.getElementById('num51').value)
+	num52 = parseInt(document.getElementById('num52').value)
+	num53 = parseInt(document.getElementById('num53').value)
+	num54 = parseInt(document.getElementById('num54').value)
+	num55 = parseInt(document.getElementById('num55').value)
+	num56 = parseInt(document.getElementById('num56').value)
+	num57 = parseInt(document.getElementById('num57').value)
+	num58 = parseInt(document.getElementById('num58').value)
+	num59 = parseInt(document.getElementById('num59').value)
+	num60 = parseInt(document.getElementById('num60').value)
+	num61 = parseInt(document.getElementById('num61').value)
+	num62 = parseInt(document.getElementById('num62').value)
+	num63 = parseInt(document.getElementById('num63').value)
+	num64 = parseInt(document.getElementById('num64').value)
+	guardar.salvar();
+};
+var guardar ={
+	salvar : function() {
+		localStorage.setItem('actual1', num1);
+		localStorage.setItem('actual2', num2);
+		localStorage.setItem('actual3', num3);
+		localStorage.setItem('actual4', num4);
+		localStorage.setItem('actual5', num5);
+		localStorage.setItem('actual6', num6);
+		localStorage.setItem('actual7', num7);
+		localStorage.setItem('actual8', num8);
+		localStorage.setItem('actual9', num9);
+		localStorage.setItem('actual10', num10);
+		localStorage.setItem('actual11', num11);
+		localStorage.setItem('actual12', num12);
+		localStorage.setItem('actual13', num13);
+		localStorage.setItem('actual14', num14);
+		localStorage.setItem('actual15', num15);
+		localStorage.setItem('actual16', num16);
+		localStorage.setItem('actual17', num17);
+		localStorage.setItem('actual18', num18);
+		localStorage.setItem('actual19', num19);
+		localStorage.setItem('actual20', num20);
+		localStorage.setItem('actual21', num21);
+		localStorage.setItem('actual22', num22);
+		localStorage.setItem('actual23', num23);
+		localStorage.setItem('actual24', num24);
+		localStorage.setItem('actual25', num25);
+		localStorage.setItem('actual26', num26);
+		localStorage.setItem('actual27', num27);
+		localStorage.setItem('actual28', num28);
+		localStorage.setItem('actual29', num29);
+		localStorage.setItem('actual30', num30);
+		localStorage.setItem('actual31', num31);
+		localStorage.setItem('actual32', num32);
+		localStorage.setItem('actual33', num33);
+		localStorage.setItem('actual34', num34);
+		localStorage.setItem('actual35', num35);
+		localStorage.setItem('actual36', num36);
+		localStorage.setItem('actual37', num37);
+		localStorage.setItem('actual38', num38);
+        localStorage.setItem('actual39', num39);
+		localStorage.setItem('actual40', num40);
+		localStorage.setItem('actual41', num41);
+		localStorage.setItem('actual42', num42);
+		localStorage.setItem('actual43', num43);
+		localStorage.setItem('actual44', num44);
+		localStorage.setItem('actual45', num45);
+		localStorage.setItem('actual46', num46);
+		localStorage.setItem('actual47', num47);
+		localStorage.setItem('actual48', num48);
+		localStorage.setItem('actual49', num49);
+		localStorage.setItem('actual50', num50);
+		localStorage.setItem('actual51', num51);
+		localStorage.setItem('actual52', num52);
+		localStorage.setItem('actual53', num53);
+		localStorage.setItem('actual54', num54);
+		localStorage.setItem('actual55', num55);
+		localStorage.setItem('actual56', num56);
+		localStorage.setItem('actual57', num57);
+		localStorage.setItem('actual58', num58);
+		localStorage.setItem('actual59', num59);
+		localStorage.setItem('actual60', num60);
+		localStorage.setItem('actual61', num61);
+		localStorage.setItem('actual62', num62);
+		localStorage.setItem('actual63', num63);
+		localStorage.setItem('actual64', num64);
+	}
+};
+guardar.salvar();
+document.getElementById('num1').value = num1;
+document.getElementById('num2').value = num2;
+document.getElementById('num3').value = num3;
+document.getElementById('num4').value = num4;
+document.getElementById('num5').value = num5;
+document.getElementById('num6').value = num6;
+document.getElementById('num7').value = num7;
+document.getElementById('num8').value = num8;
+document.getElementById('num9').value = num9;
+document.getElementById('num10').value = num10;
+document.getElementById('num11').value = num11;
+document.getElementById('num12').value = num12;
+document.getElementById('num13').value = num13;
+document.getElementById('num14').value = num14;
+document.getElementById('num15').value = num15;
+document.getElementById('num16').value = num16;
+document.getElementById('num17').value = num17;
+document.getElementById('num18').value = num18;
+document.getElementById('num19').value = num19;
+document.getElementById('num20').value = num20;
+document.getElementById('num21').value = num21;
+document.getElementById('num22').value = num22;
+document.getElementById('num23').value = num23;
+document.getElementById('num24').value = num24;
+document.getElementById('num25').value = num25;
+document.getElementById('num26').value = num26;
+document.getElementById('num27').value = num27;
+document.getElementById('num28').value = num28;
+document.getElementById('num29').value = num29;
+document.getElementById('num30').value = num30;
+document.getElementById('num31').value = num31;
+document.getElementById('num32').value = num32;
+document.getElementById('num33').value = num33;
+document.getElementById('num34').value = num34;
+document.getElementById('num35').value = num35;
+document.getElementById('num36').value = num36;
+document.getElementById('num37').value = num37;
+document.getElementById('num38').value = num38;
+document.getElementById('num39').value = num39;
+document.getElementById('num40').value = num40;
+document.getElementById('num41').value = num41;
+document.getElementById('num42').value = num42;
+document.getElementById('num43').value = num43;
+document.getElementById('num44').value = num44;
+document.getElementById('num45').value = num45;
+document.getElementById('num46').value = num46;
+document.getElementById('num47').value = num47;
+document.getElementById('num48').value = num48;
+document.getElementById('num49').value = num49;
+document.getElementById('num50').value = num50;
+document.getElementById('num51').value = num51;
+document.getElementById('num52').value = num52;
+document.getElementById('num53').value = num53;
+document.getElementById('num54').value = num54;
+document.getElementById('num55').value = num55;
+document.getElementById('num56').value = num56;
+document.getElementById('num57').value = num57;
+document.getElementById('num58').value = num58;
+document.getElementById('num59').value = num59;
+document.getElementById('num60').value = num60;
+document.getElementById('num61').value = num61;
+document.getElementById('num62').value = num62;
+document.getElementById('num63').value = num63;
+document.getElementById('num64').value = num64;	
